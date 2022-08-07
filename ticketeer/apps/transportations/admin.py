fac6cccc03bf2e8c08d28953f5e222c2b43b382b
Naufal Hilmiaji <nhilmiaji@gmail.com>
2022-08-07 07:33:55 +0700

    Minor: transportation admin and gitignore

--- ticketeer/apps/transportations/admin.py
+++ ticketeer/apps/transportations/admin.py
@@ -4,7 +4,7 @@ from .models import Transportation
 
 
 class TransportationAdmin(admin.ModelAdmin):
-    list_display = ('name', 'type', 'units')
+    list_display = ('name', 'type', 'units', 'created')
     search_fields = ('name', 'type',)
 
 admin.site.register(Transportation, TransportationAdmin)
